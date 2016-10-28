48df00b1a5c20eaa41bfe740a4f1a5685a91388a
Alex Gryll <gryll@mail.ru>
2016-10-28 18:00:09 +0300

    Remove redundant method.

--- Day1/SampleService/MyServiceLibrary/MyService.cs
+++ Day1/SampleService/MyServiceLibrary/MyService.cs
@@ -1,14 +1,8 @@
-﻿using System;
-
-namespace MyServiceLibrary
+﻿namespace MyServiceLibrary
 {
     // Rename this class. Give the class an appropriate name that will allow all other developers understand it's purpose.
     public class MyService
     {
         // Add all required methods here.
-        public void Add(object p)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
