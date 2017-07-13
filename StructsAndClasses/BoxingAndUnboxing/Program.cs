c02bca65bdb5b75e70a9d5568939d44f2d9ffc56
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-07-13 08:44:44 +0300

    Move ValueAndReferenceTypes. Add BoxingAndUnboxing.

---
+++ StructsAndClasses/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleBoxingAndUnboxing.Example();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StructsAndClasses/BoxingAndUnboxing/SimpleBoxingAndUnboxing.cs
@@ -0,0 +1,12 @@
+﻿namespace BoxingAndUnboxing
+{
+    public class SimpleBoxingAndUnboxing
+    {
+        public static void Example()
+        {
+            int i = 101;
+            object o = i; // "Boxing is implicit"
+            int j = (int)o; // "Unboxing is explicit"
+        }
+    }
+}
