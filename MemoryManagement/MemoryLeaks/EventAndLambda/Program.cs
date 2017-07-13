3509584405436cf8089dca857522827f85565667
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-07-13 19:01:50 +0300

    Add MemoryLeaks.

---
+++ MemoryManagement/MemoryLeaks/EventAndLambda/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EventAndLambda
+{
+    class Program
+    {
+        public static event EventHandler OnSomethingHappened;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to exit.");
+
+            do
+            {
+                object o = new object[1000];
+
+                OnSomethingHappened += (sender, eventArgs) =>
+                {
+                    Console.WriteLine(o.GetType());
+                };
+
+                new System.Xml.Serialization.XmlSerializer(typeof(object), new Type[] { });
+            }
+            while (!Console.KeyAvailable);
+        }
+    }
+}
---
+++ MemoryManagement/MemoryLeaks/XmlSerializer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace XmlSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to exit.");
+
+            do
+            {
+                new System.Xml.Serialization.XmlSerializer(typeof(object), new Type[] { });
+            }
+            while (!Console.KeyAvailable);
+        }
+    }
+}
