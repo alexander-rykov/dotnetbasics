0611b6d70cf26b3088a5380c17dd5f9a240ca8e3
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-07-13 19:05:31 +0300

    Add MemoryLeaks.

--- MemoryManagement/MemoryLeaks/XmlSerializer/Program.cs
+++ MemoryManagement/MemoryLeaks/XmlSerializer/Program.cs
@@ -4,6 +4,8 @@ namespace XmlSerializer
 {
     class Program
     {
+        // More information about memory leak in XmlSerializer
+        // https://blogs.msdn.microsoft.com/tess/2006/02/15/net-memory-leak-xmlserializing-your-way-to-a-memory-leak/
         static void Main(string[] args)
         {
             Console.WriteLine("Press enter to exit.");
