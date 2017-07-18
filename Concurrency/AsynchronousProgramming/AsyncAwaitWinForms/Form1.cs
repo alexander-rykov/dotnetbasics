17d384b5286622d6b1231318177bc0b407cd978d
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-07-18 15:18:40 +0300

    Add AsyncAwaitWinForms.

---
+++ Concurrency/AsynchronousProgramming/AsyncAwaitWinForms/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitWinForms
+{
+    public partial class Form1 : Form
+    {
+        public class Service
+        {
+            public async Task<ServiceData> DoSomething()
+            {
+                #region GetServiceData implementation
+
+                await Task.Delay(100);
+
+                return new ServiceData
+                {
+                    MyName = "Windows Forms"
+                };
+
+                #endregion
+            }
+        }
+
+        public class ServiceData
+        {
+            public string MyName { get; set; }
+        }
+
+        private readonly Service _service = new Service();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var task = _service.DoSomething();
+            textBox1.Text = task.Result.MyName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var task = _service.DoSomething();
+            task.Wait();
+            textBox1.Text = task.Result.MyName;
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            var data = await _service.DoSomething();
+            textBox1.Text = data.MyName;
+        }
+    }
+}
