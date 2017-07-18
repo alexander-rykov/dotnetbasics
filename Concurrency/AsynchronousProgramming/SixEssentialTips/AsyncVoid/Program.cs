8235642abb4d2f269723af38a5b66b57223e6e1f
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-07-18 12:40:22 +0300

    Add RichterExamples and SixEssentialTips.

---
+++ Concurrency/AsynchronousProgramming/SixEssentialTips/AsyncVoid/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncVoid
+{
+    // NOTE This example is taken from this video: https://channel9.msdn.com/Series/Three-Essential-Tips-for-Async/Tip-1-Async-void-is-for-top-level-event-handlers-only
+    class Program
+    {
+        private string m_GetResponse;
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SendData("http://mydomain.com/api/service/users");
+                await Task.Delay(2000);
+                Debug.WriteLine("Received data {0}", m_GetResponse);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception when sending data: {0}.", e.Message);
+            }
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SendData("http://mydomain.com/api/service/users");
+                // await Task.Delay(2000);
+                // Debug.WriteLine("Received data {0}", m_GetResponse);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception when sending data: {0}.", e.Message);
+            }
+        }
+
+        private async void SendData(string url)
+        {
+            var request = WebRequest.Create(url);
+            using (var response = await request.GetResponseAsync())
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                m_GetResponse = stream.ReadToEnd();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
