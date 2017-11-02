7c5764c4110d2e1f2029cf1c3cbd109c5feec478
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-11-02 14:30:44 +0300

    Remove SampleServices, Samples folders. Remove StyleCop file.

--- SampleService/MyServiceLibrary/MyService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyServiceLibrary
-{
-    // Rename this class. Give the class an appropriate name that will allow all other developers understand it's purpose.
-    public class MyService
-    {
-        // Add all required methods here.
-    }
-}
