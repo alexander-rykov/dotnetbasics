05343c92626341a833332e75f3647970a2dc9138
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-11-02 18:06:48 +0300

    Add StringBuilderExample.

---
+++ Strings/StringBuilderExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                sb.AppendFormat("-{0} test string-", i);
+            }
+
+            Console.WriteLine(sb.Length);
+            Console.ReadLine();
+        }
+    }
+}
